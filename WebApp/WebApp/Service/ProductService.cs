2abfc89c6bd7742665ad563581e79e38d69f2c16
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2024-11-17 12:27:25 +0100

    Merged ProduktView with main (A lot of code had to be removed though because of merge errors)

---
+++ WebApp/WebApp/Service/ProductService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApp.DataAccess.Repositories;
+using WebApp.DTO;
+using WebApp.Models;
+
+namespace WebApp.Service
+{
+    public class ProductService : IProductService
+    {
+
+        public ProductDTO CreateProduct(int id, string name, TimeSpan estimatedProductionTime, Dictionary<RawMaterial, double> rawMaterialNeeded, DateTime createdAt, DateTime updatedAt, int amountInStock)
+        {
+            return ProductRepository.AddProduct(new ProductDTO(id, name, estimatedProductionTime, rawMaterialNeeded, createdAt, updatedAt, amountInStock));
+        }
+
+        public ProductDTO DeleteProduct(ProductDTO productDTO)
+        {
+            return ProductRepository.DeleteProduct(productDTO);
+        }
+
+        public IEnumerable<ProductDTO> GetAllProducts()
+        {
+            return ProductRepository.GetProducts();
+        }
+
+        public IEnumerable<ProductDTO> GetProductByName(string name)
+        {
+            return ProductRepository.GetProduct(name);
+        }
+
+        public ProductDTO UpdateProduct(ProductDTO productDTO)
+        {
+            return ProductRepository.EditProduct(productDTO);
+        }
+    }
+}
